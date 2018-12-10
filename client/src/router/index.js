1a5c968403bcb63a84975a393bad311bf8febe60
Quinten O'Neal <quintenoneal@Quintens-MacBook-Pro.local>
2018-12-09 22:45:02 -0500

    integrating the Vue.js client with Auth0

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Callback from '@/components/Callback'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/callback',
+      name: 'Callback',
+      component: Callback
     }
   ]
 })
